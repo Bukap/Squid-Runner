743255a067f5163b7426803c7c84ebcca05b5854
Bukap <piotrtrelinski3@gmail.com>
2021-11-02 19:19:09 +0100

    .gitignore

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,26 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
-public class ScoreManager : CharacterControl
+public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private int totalScore;
-    [SerializeField] private int currentScore;
-    [SerializeField] private int sprintMultiplier;
-    [SerializeField] private int sprintScore;
-    [SerializeField] private int sprintValue;
+    [SerializeField] private int totalScore;        //Total amount of points earned during all games
+    [SerializeField] private float currentScore;      //Current score during the run
+    [SerializeField] private float sprintMultiplier = 1;      //Increments the score received
+    [SerializeField] private float multiplierIncrease;      // Increases the sprintMultiplier when the player moves
+    [SerializeField] private float multiplierDecrease;      // Decreases the sprintMultiplier when the player doesn't move
+    [SerializeField] private float incrementValue;       //Amount of points earned every fixed amount of time
+    [SerializeField] private float increaseFrequency;       //Once clock will reach this number it will increase the current score acording to a formula
+    [SerializeField] private float clock;
+
+    private CharacterControl characterControl;
 
 
-    void Update()
+    void Awake()
     {
-        
+        Assert.AreEqual(1, sprintMultiplier);
+        Assert.IsTrue(multiplierIncrease>0);
+        Assert.IsTrue(multiplierDecrease>0);
+        Assert.IsTrue(incrementValue>0);
+        Assert.IsTrue(increaseFrequency>0);
+    }
+
+    void Start()
+    {
+        characterControl = GetComponent<CharacterControl>();
+    }
+
+    void FixedUpdate()
+    {
+        clock += Time.deltaTime;
+        gettingScore();
+        multiplierHandler();
     }
 
     private void gettingScore()
     {
-        switch (speed)
+        if (characterControl.speed > 0 && clock > increaseFrequency)
         {
-
+            currentScore += incrementValue * sprintMultiplier;
+            clock = 0;
         }
     }
 
+    private void multiplierHandler()
+    {
+        if(characterControl.speed>0)
+            sprintMultiplier += multiplierIncrease;
+        else if(sprintMultiplier>1)
+            sprintMultiplier -= multiplierDecrease;
+
+    }
+
+    public void AddToTotalScore()
+    {
+        totalScore +=  (int)currentScore;
+        RestartScore();
+    }
+
+    public void RestartScore()
+    {
+        currentScore = 0;
+        sprintMultiplier = 1;
+        clock = 0;
+    }
 }
